d128bf367a9cd6c1e9c66cb0b0ea6b5febc5d7ae
Robert Rozehnal <rob.kab.roz@gmail.com>
2022-03-12 22:14:21 +0100

    JS v1

--- TechnicalDocumentationPage-BS5/code.js
+++ TechnicalDocumentationPage-BS5/code.js
@@ -1,13 +1,28 @@
 const fixed = document.querySelector(".fixed-top");
+const body = document.querySelector("body");
+const main = document.querySelector(".rob-main");
+const menu = document.querySelector(".list-group");
+const htmMain = document.querySelector("html");
+const robNav = document.querySelector(".rob-nav");
 
 
 function myFunction(x){
-    if (x.matches){
-        document.body.style.backgroundColor = "yellow";
-        fixed.classList.remove("fixed-top");
+    if (x.matches){        
+        main.style.marginLeft = "350px";
+        main.classList.remove("mt-5");
+        body.style.marginTop = "0";
+        menu.style.height = "500px";
+        menu.style.overflowY = "initial";
+        htmMain.style.scrollPaddingTop = "0";
+        robNav.classList.remove("bg-white");
     }else{
-        document.body.style.backgroundColor = "pink";
-        fixed.classList.add("fixed-top");
+        main.style.marginLeft = "0";
+        main.classList.add("mt-5");
+        body.style.marginTop = "220px";
+        menu.style.height = "220px";
+        menu.style.overflowY = "scroll";
+        htmMain.style.scrollPaddingTop = "300px";
+        robNav.classList.add("bg-white");
     }
 }
 
@@ -17,4 +32,4 @@ let x = window.matchMedia("(min-width: 992px)");
 x.addEventListener('change', myFunction)
 
 
-myFunction(x)
+myFunction(x);
